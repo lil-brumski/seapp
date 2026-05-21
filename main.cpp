14a8360d1c227a248bb4e82f7aa447d15b11daaa
lil-brumski <davidbrume7@gmail.com>
2026-05-21 20:02:41 +0100

    Add argparse functions and classes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <opencv2/opencv.hpp>
-#include <filesystem>
+#include <argparse/argparse.hpp>
+#include "seapp_version.h"
 
 void ImageFn(const std::string& image);
 void VideoFn(std::string video);
 void WebCamFn();
 
 auto main(int argc, char** argv) -> int {
-    if(argc != 3) {
-        std::cerr << "Enter one argument: image location" << std::endl;
+    std::string pv = R"(seapp )"  + std::to_string(seapp_VERSION_MAJOR) + "." + std::to_string(seapp_VERSION_MINOR) + "." + std::to_string(seapp_VERSION_PATCH)
+            + "\n\n" + R"(Written by David Tamaratare Oghenebrume)";
+
+    argparse::ArgumentParser program("seapp", pv);
+
+    program.add_argument("-i", "--image")
+        .help("Displays an image")
+        .nargs(1)
+        ;//.required();
+
+    program.add_argument("-vd", "--video")
+        .help("Displays a video")
+        .nargs(1)
+        ;//.required();
+
+    try{
+        program.parse_args(argc, argv);
+    }
+    catch(const std::exception& error){
+        std::cerr << error.what() << "\n" << program << std::endl;
         return 1;
     }
 
-    std::cout << std::boolalpha << argv[2] << " exists: " << std::filesystem::exists(argv[2]) << std::endl;
+    if(program.is_used("--image")) ImageFn(program.get<std::string>("--image"));
+    if(program.is_used("--video")) VideoFn(program.get<std::string>("--video"));
 
-    ImageFn(static_cast<std::string>(argv[1]));
-    VideoFn(static_cast<std::string>(argv[2]));
     WebCamFn();
 
     return 0;
